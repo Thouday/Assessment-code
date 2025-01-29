c9b26b67216880d7e4471664dde93eff26e239d4
Pavan <thouday.pavan99@gmail.com>
2025-01-29 10:15:54 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,17 @@
+module.exports = {
+    validCredentials: {
+        firstName: 'Thouday',
+        lastName: 'Pavan Kumar',
+        email: 'thouday.pavan99@gmail.com',
+        password: 'Password123'
+    },
+    invalidCredentials: {
+        email: 'invalid@example.com',
+        password: 'wrongPassword'
+    },
+    categories: {
+            men: 'men',
+            women: 'women',
+            gear: 'gear',
+        }
+};
